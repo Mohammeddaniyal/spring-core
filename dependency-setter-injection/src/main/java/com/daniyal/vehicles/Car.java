b05fa70cf7c1de9eaea4c065ae2b358a3169eeff
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-16 17:34:47 +0530

    Handled injection of literal value

--- dependency-setter-injection/src/main/java/com/daniyal/vehicles/Car.java
+++ dependency-setter-injection/src/main/java/com/daniyal/vehicles/Car.java
@@ -2,9 +2,29 @@ package com.daniyal.vehicles;
 
 public class Car implements Vehicle{
     DiscountService discountService;
-
+    private String brandName;
     public void setDiscountService(DiscountService discountService) {
         this.discountService = discountService;
+    }
+
+    public Car()
+    {
+        // default constructor
+        // needed for bean instantiation
+        // compiler will not write this constructor, due to parameterized const. presence
+    }
+
+    public Car(DiscountService discountService) {
+        this.discountService = discountService;
+    }
+
+    public String getBrandName() {
+        return brandName;
+
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
     }
 
     @Override
