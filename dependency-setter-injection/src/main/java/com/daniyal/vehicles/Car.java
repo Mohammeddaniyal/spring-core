4f93ced965b3b4209c1ba6a8772031bae7999ae1
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-16 16:53:21 +0530

    Removed parameterized constructor

--- dependency-setter-injection/src/main/java/com/daniyal/vehicles/Car.java
+++ dependency-setter-injection/src/main/java/com/daniyal/vehicles/Car.java
@@ -3,10 +3,6 @@ package com.daniyal.vehicles;
 public class Car implements Vehicle{
     DiscountService discountService;
 
-    public Car(DiscountService discountService) {
-        this.discountService = discountService;
-    }
-
     @Override
     public String getMileage() {
         return "30 kmpl";
