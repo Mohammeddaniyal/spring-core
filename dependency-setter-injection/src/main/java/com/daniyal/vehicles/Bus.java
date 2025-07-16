9881fbaaebb09c1cfc78c8a9217941aa7a7e0cd1
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-16 16:54:14 +0530

    Added setter for Discount Service

--- dependency-setter-injection/src/main/java/com/daniyal/vehicles/Bus.java
+++ dependency-setter-injection/src/main/java/com/daniyal/vehicles/Bus.java
@@ -3,6 +3,9 @@ package com.daniyal.vehicles;
 public class Bus implements Vehicle{
     DiscountService discountService;
 
+    public void setDiscountService(DiscountService discountService) {
+        this.discountService = discountService;
+    }
 
     @Override
     public String getMileage() {
