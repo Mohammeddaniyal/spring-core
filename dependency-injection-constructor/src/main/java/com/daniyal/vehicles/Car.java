aece983b779ee4c7e696f9f41326b9e648d1a9d3
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-16 16:07:20 +0530

    Added Constructor for injection

--- dependency-injection-constructor/src/main/java/com/daniyal/vehicles/Car.java
+++ dependency-injection-constructor/src/main/java/com/daniyal/vehicles/Car.java
@@ -2,6 +2,11 @@ package com.daniyal.vehicles;
 
 public class Car implements Vehicle{
     DiscountService discountService;
+
+    public Car(DiscountService discountService) {
+        this.discountService = discountService;
+    }
+
     @Override
     public String getMileage() {
         return "30 kmpl";
