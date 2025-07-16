33c8fa857748b0e33c8b3af3e7abd9476be6989f
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-16 17:07:25 +0530

    Updated the discount message

--- dependency-setter-injection/src/main/java/com/daniyal/vehicles/Bus.java
+++ dependency-setter-injection/src/main/java/com/daniyal/vehicles/Bus.java
@@ -14,6 +14,6 @@ public class Bus implements Vehicle{
 
     @Override
     public String getDiscountMessage() {
-        return this.discountService.getDiscountMessage();
+        return "Bus : "+this.discountService.getDiscountMessage();
     }
 }
