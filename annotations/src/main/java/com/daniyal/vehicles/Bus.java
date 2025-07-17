a9e359acf586f2974fdb2afb2ce77d250f513339
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-17 14:52:02 +0530

    Introduced new property DiscountService and used @Autowired annotation to inject(setter injection)

--- annotations/src/main/java/com/daniyal/vehicles/Bus.java
+++ annotations/src/main/java/com/daniyal/vehicles/Bus.java
@@ -1,12 +1,25 @@
 package com.daniyal.vehicles;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("MyDearBus")
 public class Bus implements Vehicle {
+private DiscountService discountService;
+// setter injection via @Autowired annotation
+
+    @Autowired
+    public void setDiscountService(DiscountService discountService) {
+        this.discountService = discountService;
+    }
 
     @Override
     public String getMileage() {
         return "20kmpl";
+    }
+
+    @Override
+    public String getDiscountMessage() {
+        return "Bus : "+this.discountService.getDiscountMessage();
     }
 }
