d0a326d604a7d7a8b771d2c9d0ded6a2db72d4a5
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-18 18:49:19 +0530

    Handled @Bean

--- annotations-2/src/main/java/com/daniyal/vehicles/Bus.java
+++ annotations-2/src/main/java/com/daniyal/vehicles/Bus.java
@@ -1,11 +1,19 @@
 package com.daniyal.vehicles;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Bus implements Vehicle{
+    @Autowired
+    private DiscountService discountService;
     @Override
     public String getMileage() {
         return "20kmpl";
+    }
+
+    @Override
+    public String getDiscountMessage() {
+        return "Bus : "+this.discountService.getDiscountMessage();
     }
 }
