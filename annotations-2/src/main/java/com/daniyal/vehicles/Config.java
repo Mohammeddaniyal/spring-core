398f739da91f05089db394822936884d41b4a6ad
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-18 18:26:03 +0530

    Instead of using xml file to scan base package now we're using annotation for this purpose

---
+++ annotations-2/src/main/java/com/daniyal/vehicles/Config.java
@@ -0,0 +1,9 @@
+package com.daniyal.vehicles;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.daniyal.vehicles")
+public class Config {
+}
