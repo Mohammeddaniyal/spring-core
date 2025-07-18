a25114a148435ee357462356545365aa54d613f0
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-18 18:49:27 +0530

    Handled @Bean

--- annotations-2/src/main/java/com/daniyal/vehicles/Car.java
+++ annotations-2/src/main/java/com/daniyal/vehicles/Car.java
@@ -2,10 +2,21 @@ package com.daniyal.vehicles;
 
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class Car implements Vehicle{
+    private DiscountService discountService;
+
+    public Car(DiscountService discountService) {
+        this.discountService = discountService;
+    }
+
     @Override
     public String getMileage() {
         return "30kmpl";
+    }
+
+    @Override
+    public String getDiscountMessage() {
+        return "Car : "+this.discountService.getDiscountMessage();
     }
 }
