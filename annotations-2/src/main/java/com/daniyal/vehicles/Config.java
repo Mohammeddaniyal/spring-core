73611c3ce1f1afb08fdff6ff653f1ec59f545499
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-18 18:46:22 +0530

    Added this part of injection another bean into a class (FestivalDiscountService)

--- annotations-2/src/main/java/com/daniyal/vehicles/Config.java
+++ annotations-2/src/main/java/com/daniyal/vehicles/Config.java
@@ -1,9 +1,22 @@
 package com.daniyal.vehicles;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.daniyal.vehicles")
 public class Config {
+    // create beans using @Bean instead of component
+    // it's like something manual
+    @Bean
+    public Vehicle carr(){
+        Car car=new Car(festivalDiscountService());
+        return car;
+    }
+    @Bean
+    public DiscountService festivalDiscountService(){
+        FestivalDiscountService festivalDiscountService=new FestivalDiscountService();
+        return festivalDiscountService;
+    }
 }
