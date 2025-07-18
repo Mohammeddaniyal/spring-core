e656561f9771075d17d5b68868b9005c0daf8099
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-18 18:50:58 +0530

    Using @PropertySource

--- annotations-2/src/main/java/com/daniyal/vehicles/Config.java
+++ annotations-2/src/main/java/com/daniyal/vehicles/Config.java
@@ -3,9 +3,11 @@ package com.daniyal.vehicles;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan("com.daniyal.vehicles")
+@PropertySource("car.properties")
 public class Config {
     // create beans using @Bean instead of component
     // it's like something manual
