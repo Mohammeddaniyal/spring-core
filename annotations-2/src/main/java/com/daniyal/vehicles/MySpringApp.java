c9cb1bfbb52db0dcc99b83798af94405134aaf48
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-18 18:33:29 +0530

    Created main class

---
+++ annotations-2/src/main/java/com/daniyal/vehicles/MySpringApp.java
@@ -0,0 +1,13 @@
+package com.daniyal.vehicles;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MySpringApp {
+    public static void main(String[] args)
+    {
+        AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(Config.class);
+        Vehicle vehicle=context.getBean("car",Vehicle.class);
+        System.out.println(vehicle.getMileage());
+    }
+}
