99d11a35f45aaaef20f2571bbf9c63a06e54b57a
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-18 15:16:37 +0530

    TO resolve the ambugity error, used @Qualifier along with @AutoWired

--- annotations/src/main/java/com/daniyal/vehicles/THAR.java
+++ annotations/src/main/java/com/daniyal/vehicles/THAR.java
@@ -1,6 +1,7 @@
 package com.daniyal.vehicles;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 // spring theory if class name first and second letter are both upper case
@@ -13,6 +14,7 @@ public class THAR implements Vehicle{
     // showing injection using @Autowired
     // field injection
     @Autowired
+    @Qualifier("WWEDiscountService")
     private DiscountService discountService;
 
     @Override
