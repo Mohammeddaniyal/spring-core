669d25e56dcb17428bc5f51e25407c8689ff8abc
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-18 15:28:31 +0530

    USed @Value annotation

--- annotations/src/main/java/com/daniyal/vehicles/Car.java
+++ annotations/src/main/java/com/daniyal/vehicles/Car.java
@@ -2,12 +2,16 @@ package com.daniyal.vehicles;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 // here in this case without annotation value
 // the bean id for the class will be default to Car --> car
 @Component
 public class Car implements Vehicle{
+
+    @Value("${car.mrp}")
+    private int mrp;
 private DiscountService discountService;
 // constructor injection via @Autowired
 @Autowired
@@ -22,5 +26,9 @@ public Car(@Qualifier("festivalDiscountService") DiscountService discountService
 
     public String getDiscountMessage() {
         return "Car : "+this.discountService.getDiscountMessage();
+    }
+
+    public int getMrp() {
+        return mrp;
     }
 }
--- annotations/src/main/java/com/daniyal/vehicles/MySpringApp.java
+++ annotations/src/main/java/com/daniyal/vehicles/MySpringApp.java
@@ -5,11 +5,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MySpringApp {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("config.xml");
-        Vehicle vehicle1=context.getBean("car",Vehicle.class);
+        Car vehicle1=context.getBean("car",Car.class);
         Vehicle vehicle2=context.getBean("MyDearBus", Vehicle.class);
         Vehicle vehicle3=context.getBean("THAR", Vehicle.class);
         System.out.println(vehicle1.getMileage());
         System.out.println(vehicle1.getDiscountMessage());
+        System.out.println(vehicle1.getMrp());
         System.out.println(vehicle2.getMileage());
         System.out.println(vehicle2.getDiscountMessage());
         System.out.println(vehicle3.getMileage());
